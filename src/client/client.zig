c8e5de29db55a248005d773fa7a2f12229977955
Devoodie <devantecoley802@gmail.com>
2025-11-28 23:48:46 -0400

    TODO >> Figure out why player 1 isn't being dealt a second card!

--- src/client/client.zig
+++ src/client/client.zig
@@ -14,6 +14,8 @@ var screenHeight: f32 = undefined;
 var card_back_texture: rl.Texture2D = undefined;
 
 var gamestate: protocol.Gamestate = undefined;
+var ani_status: Status = Status.DEALING;
+var rendering_index: u16 = 0;
 
 pub fn main() !void {
     //1280
@@ -21,9 +23,6 @@ pub fn main() !void {
 
     //sample gamestate data
     rl.initWindow(0, 0, "Dev's Casino");
-
-    // signedScreenWidth = @divTrunc((rl.getScreenWidth() * 2), 3);
-    // signedScreenHeight = @divTrunc((rl.getScreenHeight() * 2), 3);
 
     signedScreenWidth = 1280;
     signedScreenHeight = 720;
@@ -81,12 +80,12 @@ pub fn blackjack() !void {
 
     const player_starting_positions: []const rl.Vector2 = &.{
         .{ .x = (screenWidth / 2), .y = screenHeight / 8 },
-        .{ .x = (screenWidth / 16), .y = (screenHeight / 8) * 4 },
-        .{ .x = (screenWidth / 16) * 3, .y = (screenHeight / 8) * 7 },
-        .{ .x = (screenWidth / 16) * 7, .y = (screenHeight / 8) * 7 },
-        .{ .x = (screenWidth / 16) * 11, .y = (screenHeight / 8) * 7 },
-        .{ .x = (screenWidth / 16) * 15, .y = (screenHeight / 8) * 7 },
         .{ .x = (screenWidth / 16) * 15, .y = (screenHeight / 8) * 4 },
+        .{ .x = (screenWidth / 16) * 15, .y = (screenHeight / 8) * 7 },
+        .{ .x = (screenWidth / 16) * 11, .y = (screenHeight / 8) * 7 },
+        .{ .x = (screenWidth / 16) * 7, .y = (screenHeight / 8) * 7 },
+        .{ .x = (screenWidth / 16) * 3, .y = (screenHeight / 8) * 7 },
+        .{ .x = (screenWidth / 16), .y = (screenHeight / 8) * 4 },
     };
 
     var player_hand_positions = [7]std.ArrayList(?std.ArrayList(rl.Vector2)){
@@ -119,15 +118,14 @@ pub fn blackjack() !void {
         undefined,
     };
 
-    //this is going to be a struggle because im getting zero lsp support on this
-    const positional_arrays: blackjack_positional_array = .{
+    var positional_arrays: blackjack_positional_array = .{
         .player_starting_positions = player_starting_positions,
         .card_positions = &player_hand_positions,
         .target_card_positions = &target_hand_positions,
         .transformation_vectors = &transformation_vectors,
     };
 
-    for (player_starting_positions, &player_hand_positions, &target_hand_positions, &transformation_vectors) |starting_position, *hand_positions, *target_positions, *transformations| {
+    for (&player_hand_positions, &target_hand_positions, &transformation_vectors) |*hand_positions, *target_positions, *transformations| {
         //initialize each player with the possibility of 3 hands
         hand_positions.* = try std.ArrayList(?std.ArrayList(rl.Vector2)).initCapacity(allocator, 3);
         target_positions.* = try std.ArrayList(?std.ArrayList(rl.Vector2)).initCapacity(allocator, 3);
@@ -135,33 +133,32 @@ pub fn blackjack() !void {
 
         //append hand positions array
         try hand_positions.*.append(allocator, try std.ArrayList(rl.Vector2).initCapacity(allocator, 4));
-        try hand_positions.items[0].?.append(allocator, .{ .x = (screenWidth / 8) * 3, .y = (screenHeight / 8) });
+        //_ = starting_position;
 
         try target_positions.*.append(allocator, try std.ArrayList(rl.Vector2).initCapacity(allocator, 4));
-        try target_positions.items[0].?.append(allocator, starting_position);
 
         try transformations.*.append(allocator, try std.ArrayList(rl.Vector2).initCapacity(allocator, 4));
-        try transformations.*.items[0].?.append(allocator, .{ .x = 0, .y = 0 });
     }
+
+    const rendered_cards: []?std.ArrayList(std.ArrayList(deck_utils.cards)) = try allocator.alloc(?std.ArrayList(std.ArrayList(deck_utils.cards)), 7);
 
     //intialize test card values
     //for player
-    for (gamestate.hands) |*player| {
+    for (gamestate.hands, rendered_cards) |*player, *rendering| {
         //initialize player
         //append a hand to the player
         //append card to hand
         player.* = try std.ArrayList(std.ArrayList(deck_utils.cards)).initCapacity(allocator, 3);
+        rendering.* = try std.ArrayList(std.ArrayList(deck_utils.cards)).initCapacity(allocator, 3);
+
         try player.*.?.append(allocator, try std.ArrayList(deck_utils.cards).initCapacity(allocator, 4));
+        try rendering.*.?.append(allocator, try std.ArrayList(deck_utils.cards).initCapacity(allocator, 4));
+
         try player.*.?.items[0].append(allocator, deck_utils.cards.SPADE_KING);
+        try player.*.?.items[0].append(allocator, deck_utils.cards.SPADE_QUEEN);
     }
 
-    gamestate.hands[1].?.items[0].appendAssumeCapacity(deck_utils.cards.SPADE_ACE);
-    positional_arrays.target_card_positions[1].items[0].?.appendAssumeCapacity(.{
-        .x = player_starting_positions[1].x + 16.0,
-        .y = player_starting_positions[1].y - 64.0,
-    });
-    positional_arrays.card_positions[1].items[0].?.appendAssumeCapacity(.{ .x = (screenWidth / 8) * 3, .y = (screenHeight / 8) });
-    positional_arrays.transformation_vectors[1].items[0].?.appendAssumeCapacity(.{ .x = 0, .y = 0 });
+    gamestate.hands[1].?.items[0].items[1] = deck_utils.cards.SPADE_ACE;
 
     defer {
         for (&player_hand_positions) |*position| {
@@ -170,15 +167,17 @@ pub fn blackjack() !void {
     }
 
     while (!rl.windowShouldClose()) {
+        if (!rl.isWindowReady()) continue;
         rl.beginDrawing();
         defer rl.endDrawing();
         rl.clearBackground(.white);
-
         //SETUP THE RECTANGLES FOR EACH PLAYER
 
         rl.drawTexture(background_texture, 0, 0, .white);
         const screenHeightDivision = @divFloor(signedScreenHeight, 4);
         const screenWidthDivision = @divFloor(signedScreenWidth, 8);
+
+        handleStatus(&positional_arrays, rendered_cards);
 
         rl.drawLine(0, screenHeightDivision * 1, signedScreenWidth, screenHeightDivision * 1, .red);
         rl.drawLine(0, screenHeightDivision * 2, signedScreenWidth, screenHeightDivision * 2, .red);
@@ -220,7 +219,114 @@ pub fn blackjack() !void {
         }
         renderDeck(0, &drawing_rectangle);
 
-        try renderCards(positional_arrays.card_positions, gamestate.hands, &drawing_rectangle);
+        for (positional_arrays.card_positions, rendered_cards) |hands, cards| {
+            for (hands.items, cards.?.items) |hand_positions, hand_cards| {
+                if (hand_positions == null) continue;
+                try renderHand(hand_positions.?, hand_cards, &drawing_rectangle);
+            }
+        }
+    }
+}
+
+//ASSERTIONS:
+//positional arrays must be equal in length
+//a players cards must be equal to the amount of positions
+pub fn handleStatus(
+    positonal_arrays: *blackjack_positional_array,
+    rendered_cards: []?std.ArrayList(std.ArrayList(deck_utils.cards)),
+) void {
+    switch (ani_status) {
+        Status.DEALING => {
+            //ensure that the first card is dealt
+            dealCards(positonal_arrays, rendered_cards);
+        },
+        Status.RESULT => {
+            //add winning effects
+        },
+    }
+}
+
+pub fn dealCards(positional_arrays: *blackjack_positional_array, rendered_cards: []?std.ArrayList(std.ArrayList(deck_utils.cards))) void {
+    var comp_index: u16 = 0;
+    //append first card
+    if (rendered_cards[1].?.items[0].items.len == 0) {
+        const first_card = gamestate.hands[1].?.items[0].items[0];
+        rendered_cards[1].?.items[0].appendAssumeCapacity(first_card);
+        positional_arrays.card_positions[1].items[0].?.appendAssumeCapacity(.{
+            .x = (screenWidth / 8) * 3,
+            .y = (screenHeight / 8),
+        });
+        positional_arrays.target_card_positions[1].items[0].?.appendAssumeCapacity(.{
+            .x = positional_arrays.player_starting_positions[1].x,
+            .y = positional_arrays.player_starting_positions[1].y,
+        });
+        positional_arrays.transformation_vectors[1].items[0].?.appendAssumeCapacity(.{
+            .x = 0,
+            .y = 0,
+        });
+    }
+    var card_offset: u8 = 0;
+    var signed_offset: f32 = 0;
+    outer: for (1..15) |dividend| {
+        const player_index = dividend % 7;
+        if (dividend > 7) {
+            card_offset = 1;
+            signed_offset = 1;
+        }
+        if (gamestate.hands[player_index] == null) continue;
+        const position = positional_arrays.card_positions[player_index].items[0].?.items[card_offset];
+        const desired_position = positional_arrays.target_card_positions[player_index].items[0].?.items[card_offset];
+
+        const x_eql = std.math.approxEqRel(f32, position.x, desired_position.x, 0.01);
+        const y_eql = std.math.approxEqRel(f32, position.y, desired_position.y, 0.01);
+        if (comp_index == rendering_index) {
+            if (x_eql and y_eql) {
+                //append next card, card position, and desired position
+                var new_card: deck_utils.cards = undefined;
+                if (dividend == 14) {
+                    ani_status = Status.RESULT;
+                    return;
+                }
+                for (player_index + 1..15) |j| {
+                    if (gamestate.hands[j % 7] == null) {
+                        continue;
+                    } else {
+                        new_card = gamestate.hands[j % 7].?.items[0].items[card_offset];
+                        rendered_cards[j % 7].?.items[0].appendAssumeCapacity(new_card);
+                        positional_arrays.card_positions[j % 7].items[0].?.appendAssumeCapacity(.{
+                            .x = (screenWidth / 8) * 3,
+                            .y = (screenHeight / 8),
+                        });
+                        if (dividend == 13) {
+                            positional_arrays.target_card_positions[j % 7].items[0].?.appendAssumeCapacity(.{
+                                .x = positional_arrays.player_starting_positions[j % 7].x + signed_offset * 16,
+                                .y = positional_arrays.player_starting_positions[j % 7].y + signed_offset * 64,
+                            });
+                        } else {
+                            positional_arrays.target_card_positions[j % 7].items[0].?.appendAssumeCapacity(.{
+                                .x = positional_arrays.player_starting_positions[j % 7].x + signed_offset * -16,
+                                .y = positional_arrays.player_starting_positions[j % 7].y + signed_offset * -64,
+                            });
+                        }
+                        positional_arrays.transformation_vectors[j % 7].items[0].?.appendAssumeCapacity(.{
+                            .x = 0,
+                            .y = 0,
+                        });
+
+                        std.debug.assert(positional_arrays.target_card_positions[j % 7].items[0].?.items.len == positional_arrays.card_positions[j % 7].items[0].?.items.len);
+                        std.debug.assert(rendered_cards[j % 7].?.items[0].items.len == positional_arrays.card_positions[j % 7].items[0].?.items.len);
+
+                        rendering_index += 1;
+                        break :outer;
+                    }
+                }
+            } else {
+                std.debug.assert(positional_arrays.target_card_positions[player_index].items[0].?.items.len == positional_arrays.card_positions[player_index].items[0].?.items.len);
+                std.debug.assert(rendered_cards[player_index].?.items[0].items.len == positional_arrays.card_positions[player_index].items[0].?.items.len);
+                break :outer;
+            }
+        }
+        comp_index += 1;
     }
 }
 
@@ -238,7 +344,7 @@ pub fn calcTransforms(
         if (x_eql and y_eql) {
             continue;
         }
-        const buffer = rl.math.vector2MoveTowards(card_position, desired_position, 10.0);
+        const buffer = rl.math.vector2MoveTowards(card_position, desired_position, 20.0);
         trans_vector.x = buffer.x - card_position.x;
         trans_vector.y = buffer.y - card_position.y;
     }
@@ -253,11 +359,12 @@ pub fn moveCards(card_positions: []rl.Vector2, trans_vectors: []rl.Vector2) void
     }
 }
 
-// this needs to strictly render cards according to position
+// STRICTLY RENDERS A HAND
 // 3Dimensions of iteration
-pub fn renderCards(
-    player_hand_positions: []std.ArrayList(?std.ArrayList(rl.Vector2)),
-    player_cards: []?std.ArrayList(std.ArrayList(deck_utils.cards)),
+// MAKE THIS GENERIC
+pub fn renderHand(
+    player_hand_positions: std.ArrayList(rl.Vector2),
+    player_cards: std.ArrayList(deck_utils.cards),
     rectangle_pointer: *rl.Rectangle,
 ) !void {
     //CARD OFFSET
@@ -265,36 +372,25 @@ pub fn renderCards(
     //HEIGHT 8
     var drawing_rectangle = rectangle_pointer.*;
 
-    //right now this just iterates over positions but lets try to have it iterate over cards too
-    //for player
-
     var card_texture: rl.Texture2D = undefined;
-    for (player_hand_positions, 0..) |player_hand, player_index| {
-        if (player_cards[player_index] == null) continue;
-        //for each player hand
+    for (player_hand_positions.items, player_cards.items) |card_position, card| {
+        card_texture = assets.card_textures[@intFromEnum(card)];
+        drawing_rectangle.x = card_position.x;
+        drawing_rectangle.y = card_position.y;
 
-        for (player_hand.items, player_cards[player_index].?.items) |hand_position, hand_cards| {
-            if (hand_position == null) continue;
-            for (hand_position.?.items, hand_cards.items) |card_position, card_value| {
-                card_texture = assets.card_textures[@intFromEnum(card_value)];
-                drawing_rectangle.x = card_position.x;
-                drawing_rectangle.y = card_position.y;
-
-                rl.drawTexturePro(
-                    card_texture,
-                    .{
-                        .x = 0,
-                        .y = 0,
-                        .width = @floatFromInt(card_texture.width),
-                        .height = @floatFromInt(card_texture.height),
-                    },
-                    drawing_rectangle,
-                    .{ .y = drawing_rectangle.height / 2.0, .x = drawing_rectangle.width / 2.0 },
-                    0,
-                    .white,
-                );
-            }
-        }
+        rl.drawTexturePro(
+            card_texture,
+            .{
+                .x = 0,
+                .y = 0,
+                .width = @floatFromInt(card_texture.width),
+                .height = @floatFromInt(card_texture.height),
+            },
+            drawing_rectangle,
+            .{ .y = drawing_rectangle.height / 2.0, .x = drawing_rectangle.width / 2.0 },
+            0,
+            .white,
+        );
     }
 }
 
@@ -332,8 +428,9 @@ const GAME = enum {
     BLACKJACK,
 };
 
-const Animations = enum {
+const Status = enum {
     DEALING,
+    RESULT,
 };
 
 const blackjack_positional_array = struct {
